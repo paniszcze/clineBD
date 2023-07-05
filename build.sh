a6ca20a8915259994d519598e4f9af1e195f83f7
danny <bahuko@gmail.com>
2023-07-05 23:44:34 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+CFLAGS="-O3 -Wall -Wextra `pkg-config --cflags raylib`"
+LIBS="`pkg-config --libs raylib`"
+
+mkdir -p ./build
+
+clang $CFLAGS -o ./build/clineBD clineBD.c $LIBS
